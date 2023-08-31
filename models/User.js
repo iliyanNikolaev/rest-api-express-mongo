12a40aff46bd470403db95f3e111043272123ac4
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-08-31 21:35:17 +0300

    default profile and cover pic in user model

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const { Schema, model, Types } = require('mongoose');
 const userSchema = new Schema({
     username: { type: String, required: true, min: 3, max: 20, unique: true },
     password: { type: String, required: true, min: 6, max: 30 },
-    profilePicture: { type: String, default: '' },
-    coverPicture: { type: String, default: '' },
+    profilePicture: { type: String, default: '/assets/profile-pic.png' },
+    coverPicture: { type: String, default: '/assets/empty-cover.png' },
     followers: { type: [Types.ObjectId], default: [], ref: 'User' },
     followings: { type: [Types.ObjectId], default: [], ref: 'User'  }
 }, { timestamps: true});
