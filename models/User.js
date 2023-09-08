7ea3201ba0e279738d0edba0b27cced07a6dd1eb
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-09-08 19:19:28 +0300

    change pictures default values in user model

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const { Schema, model, Types } = require('mongoose');
 const userSchema = new Schema({
     username: { type: String, required: true, min: 3, max: 20, unique: true },
     password: { type: String, required: true, min: 6, max: 30 },
-    profilePicture: { type: String, default: '/assets/profile-pic.png' },
-    coverPicture: { type: String, default: '/assets/empty-cover.png' },
+    profilePicture: { type: String, default: 'https://res.cloudinary.com/dwq3ysahj/image/upload/v1694182765/profile-pic_ekruoe.png' },
+    coverPicture: { type: String, default: 'https://res.cloudinary.com/dwq3ysahj/image/upload/v1694182765/empty-cover_acce52.png' },
     followers: { type: [Types.ObjectId], default: [], ref: 'User' },
     followings: { type: [Types.ObjectId], default: [], ref: 'User'}
 }, { timestamps: true});
